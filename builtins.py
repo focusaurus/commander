f07f27853cb9840e56aed0f9fe913043eda57829
Peter Lyons <pete@peterlyons.com>
2017-09-09 19:31:26 -0600

    Update help to be more verbose

--- builtins.py
+++ builtins.py
@@ -6,10 +6,19 @@ from helpers import noclear
 @engine.command(alias="?")
 @noclear
 def help():
-    keys = engine.commands().keys()
+    commands = engine.commands()
+    keys = commands.keys()
     keys.sort()
     print 'Commander. CTRL-D or "quit" to quit. Available commands: '
-    print "\n".join(keys)
+    for key in keys:
+        func = commands[key]
+        label = key
+        words = [key]
+        if key is not func.func_name:
+            words.extend([" (alias of ", func.func_name, ")"])
+        if func.func_doc:
+            words.extend([" ", func.func_doc])
+        print("".join(words))
 
 
 @engine.command(alias="q")
